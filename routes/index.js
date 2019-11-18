9b61553423d59c54290f30ae4ddf0fbe630f9916
ChenGary <cgc2520071@hotmail.com>
2019-11-19 02:53:24 +0800

    'dev'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express -HSY' });
+  res.render('index', { title: 'Express -HSY env' });
 });
 
 module.exports = router;
